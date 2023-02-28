a3f4f156604df9087b35f98958503ecd1272eb5b
5r1 <68501681+5r100@users.noreply.github.com>
2023-02-28 21:12:27 +0530

    back to proper code

--- app.js
+++ app.js
@@ -5,21 +5,20 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res)=>{
-    var string= "not readed any data";
-    var server = net.createServer(function(socket) {
-        // confirm socket connection from client
-        console.log((new Date())+'A client connected to server...');
-        socket.on('data', function(data) {
-            string = data.toString();
-            console.log(string)
-        });
-        // send info to client
-        socket.pipe(socket);
-        socket.end();
-        console.log('The client has disconnected...\n');
-    }).listen(502, '192.168.1.60');
-    res.set('Content-Type','text/html');
-    res.status(200).send("<h1> readed : "+string+"</h1>");
+    var string = "Not received any data";
+    var client = new net.Socket();
+
+    client.connect(502,'192.168.1.60',function(){
+        console.log('connected');
+    })
+	
+    client.on('data',function(data){
+        string = data.toString()
+        console.log('Received : '+string)
+    })
+    console.log('readed weight : ',string)
+    res.set('Content-Type', 'text/html');
+    res.status(200).send("<h1>Readed weight : "+string+"</h1>");
 });
 
 app.listen(PORT, (error) =>{
