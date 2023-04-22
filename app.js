00763bdbd19e0a5569304dacf10f34fa13bb0e97
5r1 <68501681+5r100@users.noreply.github.com>
2023-04-22 10:02:27 +0530

    set as function

--- app.js
+++ app.js
@@ -4,25 +4,37 @@ var net     = require('net')
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res)=>{
-    console.log('new request received');
-    client = net.connect({ port: 502, host: '192.168.1.60' }, function () {
-        console.log('connected');
-        res.set('Content-Type', 'text/html');
-        res.status(200).send("<h1>connected</h1>");
-    })
-        
-    client.on('data',function(data){
-        console.log('Received : '+ data.toString());
-        res.set('Content-Type', 'text/html');
-        res.status(200).send("<h1>data : "+data.toString()+"</h1>");
-    })
+var client=[];
+
+function tcp_connection(ip_address)
+{
+    console.log("connecting....");
+    client[ip_address] = net.connect({port: 502, host: ip_address}, function() {
+      console.log('Connection established! '+ip_address);
+    });  
     
-    client.on('error', function(ex){
+    client[ip_address].on('end', function() {
+      console.log('Disconnected :('+ ip_address);               
+    });
+
+    client[ip_address].on('error', function(ex) {
         console.log(ex);
         res.set('Content-Type', 'text/html');
         res.status(200).send("<h1>ERROR</h1>");
-    })
+    });
+    
+    client[ip_address].on('data', function(data)
+    {
+        var debug_data = data.toString();  
+        console.log(debug_data);
+        res.set('Content-Type', 'text/html');
+        res.status(200).send("<h1>data : "+data.toString()+"</h1>");
+    });
+}
+
+app.get('/', (req, res)=>{
+    console.log('new request received');
+    tcp_client("192.168.1.60");
 });
 
 app.listen(PORT, (error) =>{
